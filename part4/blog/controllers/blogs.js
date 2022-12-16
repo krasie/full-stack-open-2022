fa362d0eb7a9e2661afb2dfed7dff01bd722cf5b
krasie <13533183561@qq.com>
2022-12-16 10:23:24 +0800

    finish exercise 4.12

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,23 +1,23 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
-  })
-  
-blogsRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-        response.status(201).json(result)
-        })
+blogsRouter.get('/', async(request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs)
 })
+
+blogsRouter.get('/:id', async(request, response) => {
+  const id = request.params.id
+  const blogs = await Blog.findById(id)
+  response.json(blogs)
+})
+  
+blogsRouter.post('/', async (request, response) => {
+    const blog = new Blog(request.body)
+    const resp = await blog.save()
+    response.status(201).json(resp)
+})
+
 
 
 module.exports = blogsRouter
---
+++ part4/blog/tests/blog_api.test.js
@@ -0,0 +1,134 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+
+const blogs = [
+  {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0
+  },
+  {
+    _id: "5a422b891b54a676234d17fa",
+    title: "First class tests",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    likes: 10,
+    __v: 0
+  },
+  {
+    _id: "5a422ba71b54a676234d17fb",
+    title: "TDD harms architecture",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+    likes: 0,
+    __v: 0
+  },
+  {
+    _id: "5a422bc61b54a676234d17fc",
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    likes: 2,
+    __v: 0
+  }  
+]
+
+const  oneBlog = {
+  title: "Type wars",
+  author: "Robert C. Martin",
+  url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+  likes: 1
+}
+
+
+const  noHasLikes = {
+  title: "Type wars",
+  author: "Robert C. Martin",
+  url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html"
+}
+
+const  noHasTitle = {
+  author: "Robert C. Martin",
+  url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html"
+}
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(blogs)
+})
+
+const  noHasUrl = {
+  title: "Type wars",
+  author: "Robert C. Martin"
+}
+
+
+test('Blogs return the correct amount', async () => {
+  const resp = await api.get('/api/blogs')
+  const blogLength = resp.body.length
+  
+  expect(blogLength).toBe(6)
+})
+
+
+test('ID is defined', async () => {
+  const resp = await api.get('/api/blogs')
+  resp.body.map(
+    blog => {
+      expect(blog.id).toBeDefined()
+    }
+  )
+})
+
+
+
+test('Add a blog', async () => {
+  const resp = await api.get('/api/blogs')
+  await api.post('/api/blogs').send(oneBlog).expect(201).expect('Content-Type', /application\/json/)
+  const newResp = await api.get('/api/blogs')
+  expect(newResp.body).toHaveLength(resp.body.length + 1)
+},20000)
+
+
+test('When likes is not defined,return 0', async () => {
+  const newBlog = noHasLikes
+  const resp = await api.post('/api/blogs').send(newBlog).expect(201).expect('Content-Type', /application\/json/)
+  const id = resp.body.id
+  const newResp = await api.get(`/api/blogs/${id}`)
+  const likes = Number(newResp.body.likes)
+  expect(likes).toBe(0)
+},20000)
+
+test('When title or url is not defiend ,return 400 request', async () => {
+  await api.post('/api/blogs').send(noHasTitle).expect(400)
+  await api.post('/api/blogs').send(noHasUrl).expect(400)
+},20000)
+
+afterAll(() => {
+  mongoose.connection.close()
+})
