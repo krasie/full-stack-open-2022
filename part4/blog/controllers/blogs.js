ff2d50e9f66a21a0f17628ea9dc9b1a2f162ecee
krasie <13533183561@qq.com>
2022-12-26 10:20:45 +0800

    finish exercises 4.19

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,6 +1,16 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 
 blogsRouter.get('/', async(request, response) => {
@@ -15,9 +25,16 @@ blogsRouter.get('/:id', async(request, response) => {
 })
   
 blogsRouter.post('/', async (request, response) => {
-    const user = await User.findById(request.body.userId)
+
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
     console.log(request.body.userId)
-    const blog = new Blog({...request.body,user:user.id})
+    const blog = new Blog({...request.body,user:user.id,author:user.name})
     const resp = await blog.save()
     user.blogs = user.blogs.concat(resp.id)
     await user.save()
