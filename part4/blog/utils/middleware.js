15ae5ec35ed97baa959a1dd5b40e96618ad115bf
krasie <13533183561@qq.com>
2022-12-26 10:48:40 +0800

    finish exercise 4.20

--- part4/blog/utils/middleware.js
+++ part4/blog/utils/middleware.js
@@ -1,3 +1,11 @@
+const tokenExtractor = (request ,response,next) =>{
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.substring(7)
+  }
+  next()
+}
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
@@ -15,5 +23,6 @@ const errorHandler = (error, request, response, next) => {
 
 module.exports = {
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor
 }
