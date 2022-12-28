4b1aca0508f94b341b9ca345f8939c45a644aee6
krasie <13533183561@qq.com>
2022-12-28 16:23:17 +0800

    fix bug

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -20,9 +20,10 @@ blogsRouter.post('/', async (request, response) => {
     if (!request.user || !request.token) {
       return response.status(401).json({ error: 'token missing or invalid' })
     }
-
     const user = request.user
-    const blog = new Blog({...request.body,user:user.id,author:user.name})
+    console.log(request.body.author)
+    const author = request.body.author ?  request.body.author : user.username
+    const blog = new Blog({...request.body,user:user.id,author:author})
     const resp = await blog.save()
     user.blogs = user.blogs.concat(resp.id)
     await user.save()
